6a7b62fa077be09cd15f9afce0ed0fb39fe4325f
sprudello <110892985+sprudello@users.noreply.github.com>
2025-03-21 15:16:11 +0100

    Added Diagram and Penalty Model

---
+++ tag-api/Models/Penalty.cs
@@ -0,0 +1,8 @@
+﻿namespace tag_api.Models
+{
+    public class Penalty
+    {
+        public int Id { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+}
--- tag-api/Models/TransportationType.cs
+++ tag-api/Models/TransportationType.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
         public required int PricePerMinute { get; set; }
     }
 }
