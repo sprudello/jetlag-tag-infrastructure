296a3bbd01e8510b42a07a4ef8774e72bc8be5e3
Julius V. Burlet <110892985+sprudello@users.noreply.github.com>
2025-03-30 22:37:42 +0200

    cors

--- tag-api/tag-api/Program.cs
+++ tag-api/tag-api/Program.cs
@@ -16,6 +16,17 @@ namespace tag_api
             builder.Services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // Add CORS policy that allows any origin, method, and header.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -31,20 +42,24 @@ namespace tag_api
 
             //app.UseHttpsRedirection();
 
+            // Apply CORS policy globally
+            app.UseCors("AllowAll");
+
             app.UseAuthorization();
 
             app.MapControllers();
 
             // Apply any pending migrations at startup.
-            /*using (var scope = app.Services.CreateScope())
+            using (var scope = app.Services.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
                 dbContext.Database.Migrate();
-            }*/
+            }
 
             app.Run();
         }
     }
 }
+
 //Server=docker_sqlserver, 1433; Database=product_db; User Id=sa; Password=myPassword1!; TrustServerCertificate=True;
 //Server=localhost;Database=tagDb;Trusted_Connection=true;TrustServerCertificate=true;
