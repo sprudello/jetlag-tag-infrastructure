b8cba988daca4a5b3c11bb78a359730e20e77800
Julius V. Burlet <110892985+sprudello@users.noreply.github.com>
2025-03-31 08:13:38 +0200

    aksjhdsa

--- tag-api/tag-api/Program.cs
+++ tag-api/tag-api/Program.cs
@@ -9,6 +9,10 @@ namespace tag_api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(8080);
+            });
             // Add services to the container.
             builder.Services.AddControllers();
 
