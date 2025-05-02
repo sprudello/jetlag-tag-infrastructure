a1e913c72e24e20d23279a76809a28261bc9cea6
Timog <110891995+timog06@users.noreply.github.com>
2025-05-02 14:10:59 +0200

    Update apiConfig.js

--- react-app/src/config/apiConfig.js
+++ react-app/src/config/apiConfig.js
@@ -1,5 +1,7 @@
 /**
- * API configuration
+ * API endpoint configuration
+ * Development: http://localhost:5296
+ * Production: https://api.sprudello.ch
  */
 const API_CONFIG = {
   BASE_URL: 'https://api.sprudello.ch'
